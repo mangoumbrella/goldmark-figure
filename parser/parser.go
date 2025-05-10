b05fcc952665c00d78075293bc6fb559b20c6549
Péter Bozsó <blotchy.demands_5u@icloud.com>
2025-05-10 23:07:17 +0200

    Add skipping single-line images

--- parser/parser.go
+++ parser/parser.go
@@ -41,6 +41,8 @@ func (b *figureParagraphTransformer) Transform(node *gast.Paragraph, reader text
 	// See CommonMark spec: https://spec.commonmark.org/0.30/#images.
 	if !imageRegexp.Match(firstLineStr) {
 		return
+	} else if lines.Len() == 1 {
+		return // Only image, no caption: skip
 	}
 	figure := fast.NewFigure()
 	node.Parent().ReplaceChild(node.Parent(), node, figure)
